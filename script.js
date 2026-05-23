3e0d324956cf7f8821c2a98947600dcec158f046
Hammad Ahmed <syedhammad283@gmail.com>
2026-05-23 22:29:27 +0500

    add cart page

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const page1Btn = document.querySelector(".btn-2");
 const page2Btn = document.querySelector(".btn-3");
 const page3Btn = document.querySelector(".btn-4");
 const nextBtn = document.querySelector(".btn-next");
+let cart = [];
 
 let newitems = [...items]
 
@@ -94,7 +95,7 @@ card.innerHTML = `
         </span>
       </div>
       
-      <button class="inline-flex items-center justify-center gap-1.5 rounded-xl bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm transition-colors duration-200 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+      <button class=" card cursor-pointer inline-flex items-center justify-center gap-1.5 rounded-xl bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm transition-colors duration-200 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
           <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
         </svg>
